09175367f398f8d497c93c4946f2f80988ffb6fa
Shahfazliz Shahron <shah.shahron@baesystems.com>
2019-05-30 12:16:26 +0800

    Update Request so that we can abort the request

--- public/js/components/Request.js
+++ public/js/components/Request.js
@@ -1,53 +1,66 @@
-(function(root, factory) {
-    
+(function (root, factory) {
+
     root.Components.Request = factory(
         root.$,
         root.Components
     );
-    
-}(window, function(
+
+}(window, function (
     $,
     Components
 ) {
     'use strict';
-    
+
     function Request(requestObject) {
         this.requestObject = requestObject;
+        this.jqXHR = undefined;
     }
-    
+
     Request.prototype.assign = Components.Component.prototype.assign;
-    
+
+    Request.prototype.abort = function () {
+        this
+            .jqXHR
+            .abort();
+    };
+
     Request.prototype.get = function (successCallback, failedCallback) {
         this.successGetCallback = successCallback
             || this.successGetCallback;
-            
+
         this.failedGetCallback = failedCallback
             || this.failedGetCallback;
-            
+
         $.ajax(this.assign(this.requestObject, {
-            method: 'GET',
-            success: this.successGetCallback, 
+            beforeSend: function (jqXHR) {
+                this.jqXHR = jqXHR;
+            }.bind(this),
             error: this.failedGetCallback,
+            method: 'GET',
+            success: this.successGetCallback,
         }));
-        
+
         return this;
     };
-    
+
     Request.prototype.post = function (successCallback, failedCallback) {
         this.successPostCallback = successCallback
             || this.successPostCallback;
-            
+
         this.failedPostCallback = failedCallback
             || this.failedPostCallback;
-            
+
         $.ajax(this.assign(this.requestObject, {
-            method: 'POST',
-            success: this.successPostCallback, 
+            beforeSend: function (jqXHR) {
+                this.jqXHR = jqXHR;
+            },
             error: this.failedPostCallback,
+            method: 'POST',
+            success: this.successPostCallback,
         }));
-        
+
         return this;
     };
-    
+
     return Request;
 }));
